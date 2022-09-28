d1570986a63e7507aa57c88ee5537eb06d44c663
CodingIsaac <isaacijuo@gmail.com>
2022-09-28 10:44:39 +0000

    Updated Commit

--- contracts/savings.sol
+++ contracts/savings.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @author Isaac Ijuo
-contract Debby is ERC20 {
+contract Debby is ERC20, ReentrancyGuard {
     address owner;
     uint public constant maxTotalSupply = 30000000 * 10 ** 18;
 
